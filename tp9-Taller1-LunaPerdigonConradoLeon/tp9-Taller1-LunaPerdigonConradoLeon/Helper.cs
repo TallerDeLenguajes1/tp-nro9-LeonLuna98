e797b6763f4e9f26ef5084cb9d570632911967b9
LeonLuna98 <you@example.com>
2019-06-06 14:35:13 -0300

    Primera subida tp9

---
+++ tp9-Taller1-LunaPerdigonConradoLeon/tp9-Taller1-LunaPerdigonConradoLeon/Helper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Helpers
+{
+    public static class  SoportesParaConfiguracion
+    {
+
+        static string nuevaRutaCarpeta = @"\RutaCarpeta";
+        static string archivoConfig = "config.dat";
+        public static void CrearArchivoDeConfiguracion(string rutadelarchivo)
+        {
+
+            if (!Directory.Exists(rutadelarchivo)){
+                Directory.CreateDirectory(rutadelarchivo);
+            }
+            else {
+                //File.Copy(@"\tp9-Taller1-LunaPerdigonConradoLeon\tp9-Taller1-LunaPerdigonConradoLeon\bin\Debug", rutadelarchivo); 
+            }
+            FileStream elarchivo = File.Create(archivoConfig);//Abro el archivo y lo mando al string
+            BinaryWriter binarioescritura = new BinaryWriter(elarchivo);
+
+            binarioescritura.Write(rutadelarchivo);
+            binarioescritura.Close();
+
+        }
+        public static string LeerConfiguracion()
+        {
+            if (File.Exists(archivoConfig)) {
+                
+                BinaryReader ruta= new BinaryReader(File.Open(archivoConfig, FileMode.Open));
+                return ruta.ReadString();
+            }
+            else
+            {
+                Console.WriteLine("El archivo no existe, ingrese un nuevo nombre para crearlo");
+           
+                SoportesParaConfiguracion.CrearArchivoDeConfiguracion(nuevaRutaCarpeta);
+                BinaryReader ruta = new BinaryReader(File.Open(archivoConfig, FileMode.Open));
+                return ruta.ReadString();
+
+            }
+
+        }
+        public static void TextoAMorse(string cadena)
+        {
+            char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            string[] caracteres = { ".- ", "-... ", "-.-. ", "-.. ", ". ", "..-. ", "--. ", ".... ", ".. ", ".--- ", "-.- ", ".-.. ", "-- ", "-. ", "--- ", ".--. ", "--.- ", ".-. ", "... ", "- ", "..- ", "...- ", "-..- ", "-..- ", "-.-- ", "--.. " };
+            int j = 0;
+            while (cadena[j] != '.')
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    if (letras[i] == cadena[j])
+                    {
+                        Console.Write(caracteres[i]);
+
+                    }
+                }
+                j++;
+            }
+        }
+        public static void MorseATexto(string cadena)
+        {
+            char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            string[] caracteres = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", "-..-", "-..-", "-.--", "--.." };           
+            string[] cadamorse = cadena.Split(' ');
+            foreach(string linea in cadamorse)
+            {
+                for(int i=0; i < 26; i++)
+                {
+                    if (Equals(linea, caracteres[i]))
+                    {
+                        Console.Write(letras[i]);
+                     
+                    }
+                }
+            }
+        }
+
+
+           /* List<string> cadenamorse = new List<string>();
+           
+            foreach (char letra in cadena)
+            {
+                switch (cadena)
+                {
+                    case "a": cadenamorse.Add(@".- "); Console.Write(cadena.ToString()); break;
+                    case "b": cadenamorse.Add(@"-... "); break;
+                    case "c": cadenamorse.Add(@"-.-. "); break;
+                    case "d": cadenamorse.Add(@"-.. "); break;
+                    case "e": cadenamorse.Add(@". "); break;
+                    case "f": cadenamorse.Add(@"..-. "); break;
+                    case "g": cadenamorse.Add(@"--. "); break;
+                    case "h": cadenamorse.Add(@".... "); break;
+                    case "i": cadenamorse.Add(@".. "); break;
+                    case "j": cadenamorse.Add(@".--- "); break;
+                    case "k": cadenamorse.Add(@"-.- "); break;
+                    case "l": cadenamorse.Add(@".-.. "); break;
+                    case "m": cadenamorse.Add(@"-- "); break;
+                    case "n": cadenamorse.Add(@"-. "); break;
+                    case "o": cadenamorse.Add(@"--- "); break;
+                    case "p": cadenamorse.Add(@".--. "); break;
+                    case "q": cadenamorse.Add(@"--.- "); break;
+                    case "r": cadenamorse.Add(@".-. "); break;
+                    case "s": cadenamorse.Add(@"... "); break;
+                    case "t": cadenamorse.Add(@"- "); break;
+                    case "u": cadenamorse.Add(@"..- "); break;
+                    case "v": cadenamorse.Add(@"...- "); break;
+                    case "w": cadenamorse.Add(@".-- "); break;
+                    case "x": cadenamorse.Add(@"-..- "); break;
+                    case "y": cadenamorse.Add(@"-.-- "); break;
+                    case "z": cadenamorse.Add(@"--.. "); break;
+                    default: cadenamorse.Add(@"     "); break;
+                }
+          
+
+            }
+
+        }*/
+
+
+
+
+
+    }
+}
---
+++ tp9-Taller1-LunaPerdigonConradoLeon/tp9-Taller1-LunaPerdigonConradoLeon/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Helpers;
+namespace tp9_Taller1_LunaPerdigonConradoLeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rutadelacarpeta = @"\RutaCarpeta";
+            /*
+             SoportesParaConfiguracion.CrearArchivoDeConfiguracion(rutadelacarpeta);
+             string cad=SoportesParaConfiguracion.LeerConfiguracion();
+             Console.WriteLine(cad);
+
+             foreach(string archivo in Directory.GetFiles(Directory.GetCurrentDirectory())){
+                 if(Path.GetExtension(archivo)== ".txt" || Path.GetExtension(archivo)==".mp3")
+                 {
+                     if (!File.Exists(rutadelacarpeta + @"\" + Path.GetFileName(archivo)))
+                     {
+                         File.Copy(archivo, rutadelacarpeta + @"\"+ Path.GetFileName(archivo));
+                     }
+
+                 }
+             }
+          */
+          /*
+            Console.WriteLine("Ingrese una cadena de texto");
+            string textoamorse = Console.ReadLine();
+            Console.WriteLine("Ingrese una cadena en morse");
+            string morseatexto = Console.ReadLine();
+            */
+            string hola = @"hola.";
+            string example = @"... --- ... .-..";
+            
+            //string convertidoatexto= SoportesParaConfiguracion.MorseATexto(morseatexto);
+            //string convertidoamorse= SoportesParaConfiguracion.MorseATexto(textoamorse);
+            //System.IO.File.WriteAllText(@"\rutadelacarpeta\ElTextoEnMorse.txt", convertidoamorse);
+            //System.IO.File.WriteAllText(@"\rutadelacarpeta\LaMorseEnTexto.txt", convertidoatexto);
+            SoportesParaConfiguracion.TextoAMorse(hola);
+
+
+            SoportesParaConfiguracion.MorseATexto(example);
+            Console.ReadKey();
+        }
+       /* public static void MorseATexto(string cadena)
+        {
+            List<char> cadenamorse = new List<char>();
+
+            foreach (char letra in cadena)
+            {
+                switch (cadena)
+                {
+                    case "a":
+                        cadenamorse.Add('.');
+                        cadenamorse.Add('-');
+                        cadenamorse.Add(' ');
+
+                        ; break;
+
+
+                }
+            }
+             StringBuilder sb = new StringBuilder(cadenamorse.ToString());
+            //Console.Write(cadenamorse.ToArray().ToString());
+            Console.WriteLine(sb);
+           
+
+
+
+
+
+        } */
+
+    }
+}
